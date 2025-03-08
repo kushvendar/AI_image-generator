ae71fe19fe6d5b30842de7705d7c77b356f45043
Kush <jangidkush1234@gmail.com>
2025-03-09 00:03:16 +0530

    frontend done

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import './components/ImgGenerator'
 import ImgGenerator from './components/ImgGenerator'
 function App() {
-  return <div className=''>
+  return <div className='bg-[#0f1b21] overflow-hidden'>
     <ImgGenerator/>
   </div> 
 }
--- src/components/ImgGenerator.jsx
+++ src/components/ImgGenerator.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import default_image from '../Assets/default_image.svg'
 const ImgGenerator = () => {
   return (
-    <div>
-        <div>AI Image <span>Generator</span></div>
-        <div>
-            <div><img src={default_image} alt="ai_img" /></div>
+    <div className='flex flex-col m-auto items-center mt-[80px] mb-[20px] gap-[30px] '>
+        <div className='text-[70px] text-white font-medium pb-[20px]'>AI Image <span className='text-[#de1b89] font-semibold'>Generator</span></div>
+        <div className='flex flex-col '>
+            <div className='w-[512px] mb-[25px]'><img src={default_image} className='rounded-[15px]' alt="ai_img" /></div>
         </div>
+        <div className='flex w-[1000px] h-[65px] justify-around  items-center rounded-[50px] bg-[#1f3540]'>
+            <input type="text" className='w-[600px] h-[50px] bg-transparent text-[18px] pl-[35px] mr-[35px] placeholder:text-[#999]' placeholder='Describe what you want to see ?' />
+            <div className='flex items-center justify-center w-[300px] h-[65px] text-white rounded-[50px] text-[20px] ml-[55px] cursor-pointer bg-[#de1b89]'>Generate</div>
+            </div>
     </div>
   )
 }
