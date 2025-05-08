971428c32c5c7da018b904d0ccc77ecc2ee9337d
Kush <jangidkush1234@gmail.com>
2025-05-08 22:27:35 +0530

    route mapping

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-
-export default function page() {
+const page = () => {
   return (
     <div>
-        hi there
+      signup route
     </div>
   )
 }
+
+export default page
